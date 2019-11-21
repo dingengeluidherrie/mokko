fd7a75019c745867acf2c198f153e8d1f2689876
Machiel Janssen <majanssen@anwb.nl>
2019-11-21 17:53:23 +0100

    added __str__-method to db-models

--- mokko/databaseofzo/models.py
+++ mokko/databaseofzo/models.py
@@ -9,30 +9,54 @@ class Lidmaatschapsproducten(models.Model):
     dekking = models.CharField(max_length=200)
     user_group = models.ManyToManyField(Group)
 
+    def __str__(self):
+        return self.productnaam
+
 class OptioneleProducten(models.Model):
     productcode = models.CharField(max_length=2)
     user_group = models.ManyToManyField(Group)
+
+    def __str__(self):
+        return self.productcode
 
 class Geslachten(models.Model):
     geslacht = models.CharField(max_length=10)
     user_group = models.ManyToManyField(Group)
 
+    def __str__(self):
+        return self.geslacht
+
 class Betaaltermijnen(models.Model):
     termijn = models.CharField(max_length=10)
     user_group = models.ManyToManyField(Group)
+
+    def __str__(self):
+        return self.termijn
 
 class Productovereenkomsten(models.Model):
     overeenkomst = models.CharField(max_length=10)
     user_group = models.ManyToManyField(Group)
 
+    def __str__(self):
+        return self.overeenkomst
+
 class Kanaalspecificatiecodes(models.Model):
     spec = models.IntegerField()
     user_group = models.ManyToManyField(Group)
+
+    def __str__(self):
+        return self.spec
 
 class Businessprocessen(models.Model):
     proces = models.CharField(max_length=50)
     user_group = models.ManyToManyField(Group)
 
+    def __str__(self):
+        return self.proces
+
 class Afspraken(models.Model):
     afspraak = models.CharField(max_length=50)
     user_group = models.ManyToManyField(Group)
+
+    def __str__(self):
+        return self.afspraak
