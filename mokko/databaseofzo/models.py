f427e7f006cdf2370ce3db38f0a38c63e6b2b4e4
Machiel Janssen <majanssen@anwb.nl>
2019-11-21 17:51:50 +0100

    Added Group-field to database models

--- mokko/databaseofzo/models.py
+++ mokko/databaseofzo/models.py
@@ -1,32 +1,38 @@
 from django.db import models
-
+from django.contrib.auth.models import Group
 # Create your models here.
 
-
-# TODO: wire these motherlovers up to the user and group models provided by django
 class Lidmaatschapsproducten(models.Model):
     productnaam = models.CharField(max_length=200)
     productcode = models.CharField(max_length=200)
     graad = models.CharField(max_length=200)
     dekking = models.CharField(max_length=200)
+    user_group = models.ManyToManyField(Group)
 
 class OptioneleProducten(models.Model):
     productcode = models.CharField(max_length=2)
+    user_group = models.ManyToManyField(Group)
 
 class Geslachten(models.Model):
     geslacht = models.CharField(max_length=10)
+    user_group = models.ManyToManyField(Group)
 
 class Betaaltermijnen(models.Model):
     termijn = models.CharField(max_length=10)
+    user_group = models.ManyToManyField(Group)
 
 class Productovereenkomsten(models.Model):
     overeenkomst = models.CharField(max_length=10)
+    user_group = models.ManyToManyField(Group)
 
 class Kanaalspecificatiecodes(models.Model):
     spec = models.IntegerField()
+    user_group = models.ManyToManyField(Group)
 
 class Businessprocessen(models.Model):
     proces = models.CharField(max_length=50)
+    user_group = models.ManyToManyField(Group)
 
 class Afspraken(models.Model):
     afspraak = models.CharField(max_length=50)
+    user_group = models.ManyToManyField(Group)
